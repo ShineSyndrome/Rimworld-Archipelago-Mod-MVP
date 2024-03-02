7bc91e086e1b9e458ef6592dfebfa61cb58e0c2a
Reckoner (Robyn) <rsg500@googlemail.com>
2024-03-02 22:02:32 +0000

    putting back .dll renaming in the post-build

--- Source/post-build.ps1
+++ Source/post-build.ps1
@@ -12,6 +12,18 @@ foreach ($item in $delete) {
     }
 }
 
+#Voodoo. Required to stop errors from .dll's being loaded in the wrong order.
+$items = Get-ChildItem -Path ../assemblies
+$order = @("Harmony", "Newtonsoft", "websocket", "Archipelago.MultiClient", "RimworldArchipelago")
+Write-Output "Assemblies contains the following: $items" 
+foreach ($item in $items) { 
+    for ( $index = 0; $index -lt $order.count; $index++) {
+        if ($item.Name.StartsWith($order[$index])) {
+            $item | Rename-Item -NewName { "$index"+"_"+$_.Name };
+        }
+    }
+}
+
 if ($rw_test_mod_assembly_dest)
 {
 	robocopy "../assemblies/" $rw_test_mod_assembly_dest /E
